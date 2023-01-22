3b0d2ef787627bf1460821104895a12730b863e8
Ada Witamborska <adrianna@tonik.com>
2023-01-22 20:46:26 +0100

    fix: switch to createRoot method

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './common/styles/_shared.scss';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Routing } from './app/routes';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routing />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
+  </Provider>
 );
